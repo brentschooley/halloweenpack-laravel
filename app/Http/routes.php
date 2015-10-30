2b2b5dda0fd252bc980a417c652496ec0ade362c
Brent Schooley <schooleybrent@gmail.com>
2015-10-30 01:07:03 -0400

    case change

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,16 +18,14 @@ Route::get('/', function () {
 Route::post('/message', function()
 {
   $picture = trim(Input::get('Body'));
-  $picture = strtolower($picture);
-  //echo($picture);
 
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, "https://api-http.littlebitscloud.cc/devices/".env('CLOUDBIT_ID')."/output");
   curl_setopt($ch, CURLOPT_POST, 1);
 
-  if($picture == "ghost"){
+  if(strcasecmp($picture, "ghost") == 0){
     curl_setopt($ch, CURLOPT_POSTFIELDS, "duration_ms=2000&percent=75");
-  } elseif ($picture == "bat") {
+  } elseif (strcasecmp($picture, "bat") == 0) {
     curl_setopt($ch, CURLOPT_POSTFIELDS, "duration_ms=2000&percent=25");
   } else {
     $resp = Response::make("<Response><Message>Sorry I don't know that one. Try bat or ghost.</Message></Response>", 200);
@@ -39,10 +37,6 @@ Route::post('/message', function()
   curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.env('CLOUDBIT_TOKEN')));
   $response = curl_exec($ch);
   curl_close($ch);
-
-  //echo env('CLOUDBIT_ID');
-  //echo('\n');
-  //echo env('CLOUDBIT_TOKEN');
 
   $resp = Response::make("<Response><Message>Happy Halloween! I set the image on Brent's hackpack to a ".$picture.".</Message></Response>", 200);
   $resp->header('Content-Type', 'text/xml');
